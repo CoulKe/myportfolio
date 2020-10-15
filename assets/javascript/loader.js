9396fa2ff78e8a2a444f41ae97d72d738d14d2b3
Luteya Coulston <thatcoul@gmail.com>
2020-10-15 12:02:47 +0300

    Added loading script

---
+++ assets/javascript/loader.js
@@ -0,0 +1,15 @@
+let l = document.createElement("div");
+const textClass = "Loading_text";
+l.className = textClass;
+
+document.body.appendChild(l);
+
+let txt = document.querySelector(".Loading_text");
+let text = "Welcome";
+text.split("").forEach((letter) => {
+  l.innerHTML += `<span class = "letter" style="opacity:0;">${letter}</span>`;
+});
+setTimeout(() => {
+  txt.style =
+    "transform: scale(0); transition-duration: 500ms;transition-timing-function: linear;";
+}, 2000);
