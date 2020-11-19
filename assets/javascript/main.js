02260bf927944c5d57500b40ff9c33fc05f6cd3c
Luteya Coulston <thatcoul@gmail.com>
2020-11-19 14:48:59 +0300

    Added minifier

--- assets/javascript/main.js
+++
@@ -1,8 +0,0 @@
-const summary = document.querySelectorAll('.summary');
-let full_desc = document.querySelectorAll('.full-description');
-
-summary.forEach((s, index) => {
-    s.addEventListener('click', ()=>{
-        full_desc[index].style.display = 'block';
-    })
-})
