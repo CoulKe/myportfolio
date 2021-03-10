3ff9a34c11230b5f1ae933870b8f5b3287b91339
Luteya Coulston <thatcoul@gmail.com>
2021-03-10 19:05:22 +0300

    Removed Intersection-observer

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-require('intersection-observer')
-
 createApp(App)
     .use(router)
     .mount('#app')
