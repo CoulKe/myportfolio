63357c103cd62eeb3153701db25e5f32c5a43f30
Luteya Coulston <thatcoul@gmail.com>
2021-03-10 18:58:53 +0300

    updated babel config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,11 @@
 process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true;
 module.exports = {
-  presets: ["@vue/app"]
+  presets: [
+    ['@vue/app', {
+      polyfills: [
+        'es6.promise',
+        'es6.symbol'
+      ]
+    }]
+  ]
 };
